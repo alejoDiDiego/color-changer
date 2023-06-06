6f46231b0afeae37386e9c0cace28e7f23b00782
alejoDiDiego <alejoh2014@gmail.com>
2023-06-06 18:43:10 -0300

    gh-pages added

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [hex1, setHex1] = useState("");
+  // const [hex2, setHex2] = useState("");
+  // const [hex3, setHex3] = useState("");
+  // const [hex4, setHex4] = useState("");
+  // const [hex5, setHex5] = useState("");
+  // const [hex6, setHex6] = useState("");
+
+  const hexVal = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+  ];
+
+  function getRandomInt() {
+    return Math.floor(Math.random() * 16);
+  }
+
+  const [hex, setHex] = useState("FFFFFF");
+
+  const changeHex = () => {
+    const hex1 = hexVal[getRandomInt()];
+    const hex2 = hexVal[getRandomInt()];
+    const hex3 = hexVal[getRandomInt()];
+    const hex4 = hexVal[getRandomInt()];
+    const hex5 = hexVal[getRandomInt()];
+    const hex6 = hexVal[getRandomInt()];
+    setHex(hex1 + hex2 + hex3 + hex4 + hex5 + hex6);
+    console.log(hex);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      className="transition-all w-screen h-screen flex flex-col justify-center items-center gap-16"
+      style={{ backgroundColor: `#${hex}` }}
+    >
+      <h1 className="font-bold text-5xl ">Color changer</h1>
+      <button
+        className="bg-white  transition text-black  p-4 text-3xl rounded-lg shadow-lg shadow-black font-bold active:scale-90 hover:bg-black  hover:text-white"
+        onClick={changeHex}
+      >
+        Click me!!
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
